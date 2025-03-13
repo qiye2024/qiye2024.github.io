dac8180b2605b51c8e14a49e90fff221d6883d0a
qiye2024 <qiye2024@gmail.com>
2025-03-13 16:07:20 +0000

    Deploying to gh-pages from @ qiye2024/qiye2024.github.io@1501d793ec0d5b2819db8d8fd2aff9c934a01128 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -80,9 +80,9 @@ ninja.data = [{
           description: "Hormesis Response of Polychlorinated Biphenyls to Escherichia Coli",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-isotope-geochemistry",
-          title: 'Isotope Geochemistry',
-          description: "An Improved Method for Precise Selenium Isotope Determination",
+            },},{id: "projects-an-improved-method-for-precise-selenium-isotope-determination",
+          title: 'An Improved Method for Precise Selenium Isotope Determination',
+          description: "Isotope Geochemistry",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-limnology",
