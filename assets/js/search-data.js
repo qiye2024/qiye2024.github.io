c0a04391179ca6a94663c91649639cd9edf16525
qiye2024 <qiye2024@gmail.com>
2026-09-22 15:44:16 +0000

    Deploying to gh-pages from @ qiye2024/qiye2024.github.io@bdeebfdeafe6885d9f2f2c65d268712417622842 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -98,8 +98,8 @@ ninja.data = [{
           description: "Freshwater Nutrient Dynamics and Phytoplankton Numerical Ecology",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-oceanology",
-          title: 'Oceanology',
+            },},{id: "projects-oceanography",
+          title: 'Oceanography',
           description: "Linking Terrestrial Inputs, Costal Benthic Fluxes, and Carbon Dynamics",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
